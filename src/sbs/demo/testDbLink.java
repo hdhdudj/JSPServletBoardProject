feab6eea19b23efbe64cbc8ff5af3f466b094c70
Administrator <Administrator@SBS-179>
2019-05-28 10:55:14 +0900

    게ㅅ판

---
+++ src/sbs/demo/testDbLink.java
@@ -0,0 +1,50 @@
+package sbs.demo;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sbs.demo.util.DBUtil;
+
+@WebServlet("/test/testDbLink")
+public class testDbLink extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    DBUtil.DBLink dbLink = DBUtil.getNewDbLink();
+
+    List<Map<String, Object>> articles = dbLink.getRows("SELECT * FROM article");
+    System.out.println(articles);
+
+    articles = dbLink.getRows("SELECT * FROM article LIMIT 2");
+    System.out.println(articles);
+
+    Map<String, Object> article = dbLink.getRow("SELECT * FROM article LIMIT 1");
+    System.out.println(article);
+
+    int count = dbLink.getRowIntValue("SELECT COUNT(*) FROM article LIMIT 1");
+    System.out.println("COUNT : " + count);
+
+    String title = dbLink.getRowStringValue("SELECT title FROM article LIMIT 1");
+    System.out.println("title : " + title);
+
+    dbLink.executeQuery("INSERT INTO article SET regDate = NOW(), title = 'test 제목', body = 'test 내용';");
+    System.out.println(dbLink.getLastInsertId() + "번 글 추가됨");
+
+    dbLink.close();
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    doGet(request, response);
+  }
+
+}
